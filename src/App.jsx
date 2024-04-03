5f6c1105a38bd743e2f41b1b998d3231a646fa69
Joshua <joshuachris000@gmail.com>
2024-04-03 07:23:06 +0100

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,30 @@ import HomeSwipers from './Home/HomeSwipers.jsx'
 import Navbar from './Navbar/Navbar.jsx'
 
 function App() {
+    //practicing javascript//
+
+    //created first function
+    function lowerOneWord(str) {
+        return str.toLowerCase()
+    }
+    // created second function
+    function firstUpper(str) {
+        const [firstWord, ...otherWords] = str.split(' ')
+        return [firstWord.toUpperCase(), ...otherWords].join(' ')
+    }
+    //Function that calls other functions
+    function changedArrays(str, fn) {
+        console.log(`Transfromed string: ${fn(str)}`)
+        console.log(`Transformes by: ${fn.name}`)
+    }
+    const mulNum = (str) => {
+        return str.split(' ').join(' /') + ' ' + 5
+    }
+
+    changedArrays('Joshua is a good dev', firstUpper)
+    changedArrays('Joshua is a good dev', lowerOneWord)
+    changedArrays('Joshua is a good dev', mulNum)
+
     return (
         <div>
             <Navbar />
