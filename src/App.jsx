2adc437d8e514116e66384aa857badfc8db31640
Joshua <joshuachris000@gmail.com>
2024-04-10 08:58:32 +0100

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Home from './Home/Home.jsx'
-import HomeSwipers from './Home/HomeSwipers.jsx'
 import Navbar from './Navbar/Navbar.jsx'
 
 function App() {
@@ -40,43 +39,31 @@ function App() {
     g()
     f()
 
-    const getJsons = (url, errMsg = `This can't be found`) => {
-        return fetch(url).then((res) => {
-            if (!res.ok) throw new Error(errMsg)
-            console.log(res)
+    // .then((data) => {
+    //     console.log(data[0])
+    //     const border = data[0].borders[0]
 
-            return res.json()
-        })
-    }
-    const getCountries = (country) => {
-        getJsons(`https://restcountries.com/v3.1/name/${country}`)
-            .then((data) => {
-                console.log(data[0])
-                const border = data[0].borders[0]
+    //     return getJsons(
+    //         `https://restcountries.com/v3.1/alpha/${border}`
+    //     )
+    // })
+    // .then((borderData) => {
+    //     console.log(borderData[0])
 
-                return getJsons(
-                    `https://restcountries.com/v3.1/alpha/${border}`
-                )
-            })
-            .then((borderData) => {
-                console.log(borderData[0])
+    //     const border2 = borderData[0].borders[0]
 
-                const border2 = borderData[0].borders[0]
+    //     return getJsons(
+    //         `https://restcountries.com/v3.1/alpha/${border2}`,
+    //         `Sorry can't get data from ${border2}`
+    //     )
+    // })
+    // .then((border2Data) => {
+    //     console.log(border2Data[0])
+    // })
 
-                return getJsons(
-                    `https://restcountries.com/v3.1/alpha/${border2}`,
-                    `Sorry can't get data from ${border2}`
-                )
-            })
-            .then((border2Data) => {
-                console.log(border2Data[0])
-            })
-
-            .catch((err) => {
-                console.error(`There's an error somewhere:  ${err.message}😪`)
-            })
-    }
-    getCountries('nigeria')
+    // .catch((err) => {
+    //     console.error(`There's an error somewhere:  ${err.message}😪`)
+    // })
 
     const getLocation = () => {
         return new Promise((resolve, reject) => {
@@ -85,6 +72,79 @@ function App() {
     }
 
     getLocation().then((pos) => console.log(pos))
+
+    // const getCountries = async (country) => {
+    //     try {
+    //         const getUrl = await fetch(
+    //             `https://restcountries.com/v3.1/name/${country}`
+    //         )
+
+    //         if (!getUrl.ok)
+    //             throw new Error(`Sorry can't get countries at this point`)
+    //         // console.log(getUrl)
+    //         const newUrl = await getUrl.json()
+    //         console.log(newUrl[0])
+
+    //         const border = newUrl[0].borders[0]
+
+    //         const border1URl = await fetch(
+    //             `https://restcountries.com/v3.1/alpha/${border}`
+    //         )
+    //         if (!border1URl.ok)
+    //             throw new Error(`Sorry can't get this border at this point`)
+
+    //         console.log(border1URl)
+    //         const newBorder1URl = await border1URl.json()
+    //         console.log(newBorder1URl[0])
+    //     } catch (err) {
+    //         console.error(`${err}💀💀💀`)
+    //     }
+    // }
+    // getCountries('nigeria')
+    const getJson = (url, errMsg = `This can't be found`) => {
+        return fetch(url).then((res) => {
+            if (!res.ok) throw new Error(errMsg)
+            console.log(res)
+
+            return res.json()
+        })
+    }
+
+    const get3Countries = async (c1, c2, c3, c4) => {
+        try {
+            const [data1] = await getJson(
+                `https://restcountries.com/v3.1/name/${c1}`
+            )
+            const [data2] = await getJson(
+                `https://restcountries.com/v3.1/name/${c2}`
+            )
+            const [data3] = await getJson(
+                `https://restcountries.com/v3.1/name/${c3}`
+            )
+            const [data4] = await getJson(
+                `https://restcountries.com/v3.1/name/${c4}`
+            )
+
+            const data = await Promise.all([
+                getJson(`https://restcountries.com/v3.1/name/${c1}`),
+                getJson(`https://restcountries.com/v3.1/name/${c2}`),
+                getJson(`https://restcountries.com/v3.1/name/${c3}`),
+                getJson(`https://restcountries.com/v3.1/name/${c4}`),
+            ])
+
+            console.log([
+                data1.borders[0],
+                data2.borders[0],
+                data3.borders[0],
+                data4.borders[0],
+                data4,
+            ])
+            console.log(data)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    get3Countries('nigeria', 'benin', 'niger', 'nigeria')
     return (
         <div>
             <Navbar />
